ca12e905e1f8b290583976b26a51ec39e75b4f35
Say0911 <97503075+Say0911@users.noreply.github.com>
2022-01-11 12:18:42 +0530

    helloworldprogram

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package pack1;
+/** 
+ Document comments  */
+
+/*
+ multiline projects 
+ */
+public class Hello {
+ public static void main(String args[]) {
+	 System.out.println("Hello World");
+	 System.out.println("My name is: " + "(2+3)");
+	 //single line comments
+	 }
+}
