bb5cc4d4dd319485ecb57673c780ac085bba6065
Say0911 <97503075+Say0911@users.noreply.github.com>
2022-01-24 22:28:03 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,11 @@
+package pack2;
+
+public class Variables {
+	public static void main(String args[]) {
+		int k =7, p; // decalre the variavle k and assign value to it 
+		p = k++ + ++k ; //pst increment
+		System.out.println("p =" +p); //p=8
+//		System.out.println("k =" +p); //p=8
+	}
+
+}
